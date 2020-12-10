d16cebed639a9c4ab913ffb7acb75e8f7428d573
Genesys Rondon <genesys.merchan@gmail.com>
2020-12-10 15:04:03 -0300

    feat: adição de Builder e AssertExtension

--- test/CursoOline.DominioTest/Cursos/CursoTest.cs
+++ test/CursoOline.DominioTest/Cursos/CursoTest.cs
@@ -1,52 +1,120 @@
-﻿using ExpectedObjects;
+﻿using CursoOline.DominioTest._Builders;
+using CursoOline.DominioTest._Util;
+using ExpectedObjects;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace CursoOline.DominioTest.Cursos
 {
     public class CursoTest
     {
+        private readonly ITestOutputHelper _output;
+        private readonly string _nome;
+        private readonly double _cargaHoraria;
+        private readonly PublicoAlvo _publicoAlvo;
+        private readonly double _valor;
+        private readonly string _descricao;
+
+        public CursoTest(ITestOutputHelper output)
+        {
+            _output = output;
+            _output.WriteLine("Contrutor sendo executado");
+
+            _nome = "Informatica Básica";
+            _descricao = "Uma descricao";
+            _cargaHoraria = 80;
+            _publicoAlvo = PublicoAlvo.Estudante;
+            _valor = 950;
+        }
+
         [Fact]
         public void DeveCriarDominio() 
         {
-
+            //organização
             var cursoEsperado = new
             {
-                Nome = "Informatica Básica",
-                CargaHoraria = 80,
-                PublicoAlvo = "Estudantes",
-                Valor = 950
-
-
+                Nome = _nome,
+                CargaHoraria = _cargaHoraria,
+                PublicoAlvo = _publicoAlvo,
+                Valor = _valor,
+                Descricao = _descricao
             };
          
-
             //açaõ
-            var curso = new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor);
+            var curso = new Curso(cursoEsperado.Nome, cursoEsperado.Descricao, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo , cursoEsperado.Valor);
 
             //Assert
-
             cursoEsperado.ToExpectedObject().ShouldMatch(curso);
            
         }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+
+        public void NaoDeveCursoTerNomeInvalido(string nomeInvalido)
+        {
+          
+            Assert.Throws<ArgumentException>(() =>
+            CursoBuilder.Novo().ComNome(nomeInvalido).Build()).ComMensagem("Nome Inválido");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-100)]
+        public void NaoDeveCursoTerUmaCargaHorariaMenorQue1(double cargaHorariaInvalida)
+        {
+          
+            Assert.Throws<ArgumentException>(() =>
+            CursoBuilder.Novo().ComCargaHoraria(cargaHorariaInvalida).Build()).ComMensagem("Carga Horaria Inválida");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-100)]
+        public void NaoDeveCursoTervalorMenorQue1(double valorInvalido)
+        {          
+            Assert.Throws<ArgumentException>(() =>
+            CursoBuilder.Novo().ComValor(valorInvalido).Build()).ComMensagem("Valor Inválido");
+        }
     }
 
+    public enum PublicoAlvo
+    {
+        Estudante,
+        Universitario,
+        Empregado,
+        Empreendedor
+    }
     public class Curso
     {
-        public Curso(string nome, double cargaHoraria, string publicoAlvo, double valor)
+        public Curso(string nome,string descricao, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
         {
+
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentException("Nome Inválido");
+
+            if (cargaHoraria < 1)
+                throw new ArgumentException("Carga Horaria Inválida");
+
+            if (valor < 1)
+                throw new ArgumentException("Valor Inválido");
+
             Nome = nome;
+            Descricao = descricao;
             CargaHoraria = cargaHoraria;
             PublicoAlvo = publicoAlvo;
             Valor = valor;
         }
 
-        public string Nome { get; set; }
-        public double CargaHoraria { get; set; }
-        public string PublicoAlvo { get; set; }
-        public double Valor { get; set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; set; }
+        public double CargaHoraria { get; private set; }
+        public PublicoAlvo PublicoAlvo { get; private set; }
+        public double Valor { get; private set; }
     }
 
 }
---
+++ test/CursoOline.DominioTest/_Util/AssertExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CursoOline.DominioTest._Util
+{
+    public static class AssertExtension
+    {
+        public static void ComMensagem(this ArgumentException exception, string mensagem)
+        {
+            if (exception.Message == mensagem)
+                Assert.True(true);
+            else
+                Assert.False(true, $"Esperava a mensagem '{mensagem}'");
+        }
+    }
+}
